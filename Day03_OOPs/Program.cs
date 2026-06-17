838b268322ae5b9237f209865291c27040e4eec9
Omkar <omkadam708@gmail.com>
2026-06-17 20:31:47 +0530

    Day03_OOPs

---
+++ Day03_OOPs/Program.cs
@@ -0,0 +1,31 @@
+﻿Employee emp = new Employee();
+emp.SetSalary(5000);
+Console.WriteLine("The salary is : " + emp.GetSalary());
+Console.WriteLine($"Salary is {emp.GetSalary()}");
+
+Manager mngr = new Manager();
+mngr.SetSalary(8000);
+Console.WriteLine("The salary is : " + mngr.GetSalary());
+Console.WriteLine($"Salary is {mngr.GetSalary()}");
+
+class Employee()
+{
+    public int Id;
+    public string Name = "";
+    private decimal Salary;
+
+    public void SetSalary(decimal amount)
+    {
+        Salary = amount;
+    }
+
+    public decimal GetSalary()
+    {
+        return Salary;
+    }
+}
+
+class Manager : Employee
+{
+    public string department = "Management";
+}
