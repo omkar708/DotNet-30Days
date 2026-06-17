4b97a40b995afd1055cbb3362343db8f73eb4cf3
Omkar <omkadam708@gmail.com>
2026-06-17 17:49:16 +0530

    Add project files.

---
+++ Day01_Basics/Program.cs
@@ -0,0 +1,16 @@
+﻿int num1 = 5;
+int num2 = 10;
+int adn, subn, divn, muln;
+
+adn = num1 + num2;
+subn = num1 - num2;
+divn = num1 / num2;
+muln = num1 * num2;
+
+Console.WriteLine("addition " + adn);
+Console.WriteLine("substraction " + subn);
+Console.WriteLine("division " + divn);
+Console.WriteLine("multiplication " + muln);
+
+string getnum = "";
+getnum = Console.ReadLine();
