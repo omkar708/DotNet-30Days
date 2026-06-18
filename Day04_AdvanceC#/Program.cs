30029c1b67d474cc8f98895617837dcc6f6cdb3a
Omkar <omkadam708@gmail.com>
2026-06-18 23:35:28 +0530

    Day04_AdvanceC#

---
+++ Day04_AdvanceC#/Program.cs
@@ -0,0 +1,35 @@
+﻿
+Employee emp = new Employee("Omkar", 26, "A834");
+Console.WriteLine(emp.Name);
+Console.WriteLine(emp.Department);
+Console.WriteLine(emp.EmployeeCode);
+Console.WriteLine(emp.Age);
+Console.WriteLine(emp.Company);
+
+class Person
+{
+    public string Name { get; set; } = "";
+    public int Age { get; set; }
+
+    //created constructor for Person to set Name and Age
+    public Person(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+
+}
+
+//Inherited Employee from Person
+class Employee : Person
+{
+    public string Department = "IT";
+    public string Company = "Yash";
+    public readonly string EmployeeCode;
+
+    //get the values of name and age from base class
+    public Employee(string name, int age, string employeecode) : base(name, age)
+    {
+        EmployeeCode = employeecode;
+    }
+}
